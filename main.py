382c8c87c29e8733aa6fc08a3de80d5c4710ea3d
FlyingWorkshop <76822456+FlyingWorkshop@users.noreply.github.com>
2020-12-31 12:52:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -34,24 +34,5 @@ def trace(original):
     return arr2
 
 
-def sharpen(original):
-    grayscale = original.convert('L')
-    arr1 = np.pad(np.array(grayscale), 1)
-    arr2 = arr1.copy()
-    rows, cols = arr1.shape
-    for y in tqdm(range(1, rows - 1)):
-        for x in range(1, cols - 1):
-            sel = arr1[y - 1:y + 2, x - 1:x + 2]
-            gx = np.sum(sel * HORZ_KERNEL)
-            gy = np.sum(sel * VERT_KERNEL)
-            g_magnitude = math.sqrt(gx ** 2 + gy ** 2)
-            if g_magnitude >= THRESHOLD:
-                g_magnitude = 255
-            else:
-                g_magnitude = 0
-            arr2[y][x] = g_magnitude
-    return arr2
-
-
 if __name__ == '__main__':
     main()
